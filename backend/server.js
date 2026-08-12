e86ed6a225680d2814994bc4c72ac63d1bbf0f8d
Kishan Chandra Pandey <kishanpandey@gmail.com>
2026-08-12 12:10:19 +0530

    update production cors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT;
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://kdx-ten.vercel.app",
     credentials: true,
   }));
 
